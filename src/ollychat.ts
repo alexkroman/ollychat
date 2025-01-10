950fd94486e32b1795f4e5c190ca4764298ff27b
Alex Kroman <>
2025-01-09 22:45:18 -0800

    time to go to bed

--- src/ollychat.ts
+++ src/ollychat.ts
@@ -4,13 +4,13 @@ import { FewShotPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { SemanticSimilarityExampleSelector } from "@langchain/core/example_selectors";
 import { examples } from "./training.js";
+import { metrics} from "./training-metric.js";
 import { Annotation } from "@langchain/langgraph";
 import { createQueryExecutor, createMetricsFetcher } from './prometheus.js';
 import { DynamicStructuredTool } from "@langchain/core/tools";
 import { StateGraph } from "@langchain/langgraph";
 
 import * as dotenv from 'dotenv';
-import { create } from "domain";
 
 dotenv.config();
 
@@ -31,24 +31,26 @@ const prometheusUrl = process.env.PROMETHEUS_URL || 'http://localhost:9090'
 
 const queryPrometheus = createQueryExecutor(prometheusUrl);
 
+
+
 const exampleSelector = await SemanticSimilarityExampleSelector.fromExamples<typeof MemoryVectorStore>(
   examples,
   new OpenAIEmbeddings(),
   MemoryVectorStore, {
   k: 5,
   inputKeys: ['question']
-}
-);
+});
 
-const examplePrompt = PromptTemplate.fromTemplate(`
-Question: {question}
-PromQL: {query}`
-);
+const metricSelector = await SemanticSimilarityExampleSelector.fromExamples<typeof MemoryVectorStore>(
+  metrics,
+  new OpenAIEmbeddings(),
+  MemoryVectorStore, {
+  k: 5,
+  inputKeys: ['description']
+});
 
-const queryPromptTemplate = new FewShotPromptTemplate({
-  exampleSelector,
-  examplePrompt,
-  prefix: `Given an input question, first create a syntactically correct PromQL query to run, then look at the results of the query and return the answer.
+const queryPromptTemplate = PromptTemplate.fromTemplate(
+`Given an input question, first create a syntactically correct PromQL query to run, then look at the results of the query and return the answer.
 
 Use the following format:
 
@@ -66,13 +68,12 @@ List of available jobs:
 List of available metrics:
 {metrics}
 
-Below are a number of examples of questions and their corresponding PromQL queries.`,
-  suffix: `
-  Question: {question}
-  PromQLQuery: 
-  `,
-  inputVariables: ["question", "metrics", "nodes", "jobs"]
-});
+Below are a number of examples of questions and their corresponding PromQL queries.
+{examples}
+
+User Question: {question}
+PromQLQuery: 
+`);
 
 const getMetricsFetcher = createMetricsFetcher(prometheusUrl);
 
@@ -116,6 +117,7 @@ const getPrometheusJobsTool = new DynamicStructuredTool({
 
 const StateAnnotation = Annotation.Root({
   question: Annotation<string>,
+  examples: Annotation<string>,
   metrics: Annotation<string>,
   nodes: Annotation<string>,
   jobs: Annotation<string>,
@@ -125,12 +127,56 @@ const StateAnnotation = Annotation.Root({
   answer: Annotation<string>,
 });
 
+const getExamples = async (state: typeof StateAnnotation.State) => {
+  const examples = await exampleSelector.selectExamples({ question: state.question });
+
+  const examplePrompt = PromptTemplate.fromTemplate(`
+Example Question: {question}
+Example PromQL: {query}`
+  );
+
+  const formattedExamples = await Promise.all(
+    examples.map(example => 
+      examplePrompt.format({
+        question: example.question,
+        query: example.query
+      })
+    )
+  );
+  
+  const combinedExamples = formattedExamples.join('\n\n');
+
+  return { examples: combinedExamples };
+};
+
+
+
 const getMetrics = async (state: typeof StateAnnotation.State) => {
-  return { metrics: await prometheusMetricsTool.invoke({}) };
+  const metrics = await metricSelector.selectExamples({ question: state.question });
+
+  const metricPrompt = PromptTemplate.fromTemplate(`
+Metric: {metric}
+Description: {description}`);
+
+  const formattedMetrics = await Promise.all(
+    metrics.map(metric => 
+      metricPrompt.format({
+        metric: metric.metric,
+        description: metric.description
+      })
+    )
+  );
+  
+  const combinedMetrics = formattedMetrics.join('\n');
+
+  return { metrics: combinedMetrics };
 };
 
 const getPrometheusNodes = async (state: typeof StateAnnotation.State) => {
-  return { nodes: await getPrometheusNodesTool.invoke({}) };
+  const nodes: {
+    metric: any; nodename: string 
+}[] = await getPrometheusNodesTool.invoke({});
+  return { nodes: nodes };  
 };
 
 const getPrometheusJobs = async (state: typeof StateAnnotation.State) => {
@@ -139,10 +185,11 @@ const getPrometheusJobs = async (state: typeof StateAnnotation.State) => {
 
 const writeQueryTemplate = async (state: typeof StateAnnotation.State) => {
   const promptValue = await queryPromptTemplate.invoke({
-    question: state.question, 
+    question: state.question,
+    examples: state.examples, 
+    metrics: state.metrics,
     jobs: state.jobs, 
-    nodes: state.nodes, 
-    metrics: state.metrics
+    nodes: state.nodes
   });
   console.log(promptValue);
   const result = await structuredModel.invoke(promptValue);
@@ -169,7 +216,8 @@ const generateAnswer = async (state: typeof StateAnnotation.State) => {
     The query returned the following data:
      ${state.result}
 
-    Based on these results, briefly answer the user's question without adding extra explanation. 
+    Using only the data above answer the user's question. 
+    If the query returned "[]" you can say "No data found". 
     `;
   console.log(promptValue);
   const response = await model.invoke(promptValue);
@@ -184,13 +232,15 @@ const graphBuilder = new StateGraph({
   stateSchema: StateAnnotation,
 })
   .addNode("getMetrics", getMetrics)
+  .addNode("getExamples", getExamples)
   .addNode("getPrometheusNodes", getPrometheusNodes)
   .addNode("getPrometheusJobs", getPrometheusJobs)
   .addNode("writeQueryTemplate", writeQueryTemplate)
   .addNode("executeQuery", executeQuery)
   .addNode("generateAnswer", generateAnswer)
   .addEdge("__start__", "getMetrics")
-  .addEdge("getMetrics", "getPrometheusNodes")
+  .addEdge("getMetrics", "getExamples")
+  .addEdge("getExamples", "getPrometheusNodes")
   .addEdge("getPrometheusNodes", "getPrometheusJobs")
   .addEdge("getPrometheusJobs", "writeQueryTemplate")
   .addEdge("writeQueryTemplate", "executeQuery")
