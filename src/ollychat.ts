e726037c8f00463c287adff75161cdd2c77c2765
Alex Kroman <>
2025-02-09 13:59:56 -0800

    get basic telemetry working

--- src/ollychat.ts
+++ src/ollychat.ts
@@ -1,9 +1,11 @@
 import { HumanMessage } from "@langchain/core/messages";
 import { app } from "./workflow/index.js";
 import { config } from "./config/config.js";
+import { posthog } from "./utils/telemetry.js";
 
 export const answerQuestion = async (inputs: { question: string }) => {
   const messages = [new HumanMessage({ content: inputs.question })];
+  posthog.capture("$question", inputs);
   const result = await app.invoke({ messages }, config);
   const lastMessage = result.messages[result.messages.length - 1];
   return lastMessage.content;
---
+++ src/utils/telemetry.ts
@@ -0,0 +1,22 @@
+import { posthog } from "posthog-js";
+import * as os from "os";
+
+function getStableHostId(): string | undefined {
+  return (
+    Object.values(os.networkInterfaces())
+      .flat()
+      .find(
+        (config) =>
+          config && !config.internal && config.mac !== "00:00:00:00:00:00",
+      )?.mac || undefined
+  );
+}
+
+posthog.init("phc_ATkdmfJutLNoQvXMXCGLKDHXQYMXV00diQ8RUDdfe52", {
+  api_host: "https://us.i.posthog.com",
+  person_profiles: "always",
+});
+
+posthog.identify(getStableHostId());
+
+export { posthog };
