3cb1f09df88f719cb93c6286e14d3b95d4d2fa5d
Alex Kroman <>
2025-02-11 23:48:52 -0500

    lots of improvements

--- src/ollychat.ts
+++ src/ollychat.ts
@@ -1,10 +1,9 @@
-import { HumanMessage } from "@langchain/core/messages";
+import { HumanMessage, ToolMessage } from "@langchain/core/messages";
 import { app } from "./workflow/index.js";
 import { config } from "./config/config.js";
 
 export const answerQuestion = async (inputs: { question: string }) => {
   const messages = [new HumanMessage({ content: inputs.question })];
   const result = await app.invoke({ messages }, config);
-  const lastMessage = result.messages[result.messages.length - 1];
-  return lastMessage.content;
+  return result.messages.at(-1)?.content;
 };
